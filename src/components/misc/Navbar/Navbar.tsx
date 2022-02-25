0a2ec22d7152c3276b124646268f836743f7ee27
Cdelatorre <carlosdelatorrediaz@gmail.com>
2022-02-26 00:26:20 +0100

    add club slector page

--- src/components/misc/Navbar/Navbar.tsx
+++ src/components/misc/Navbar/Navbar.tsx
@@ -1,22 +1,28 @@
-import React, { useState, useState } from 'react';
-import { BottomNavigation, BottomNavigationAction } from '@mui/material';
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
+import React, { useState } from 'react';
+import { BottomNavigation, BottomNavigationAction, Paper } from '@mui/material';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import NewspaperIcon from '@mui/icons-material/Newspaper';
+import CircleNotificationsIcon from '@mui/icons-material/CircleNotifications';
+import DateRangeIcon from '@mui/icons-material/DateRange';
 
 const Navbar = () => {
   const [value, setValue] = useState(0);
 
   return (
-    <BottomNavigation
-      showLabels
-      value={value}
-      onChange={(event, newValue) => {
-        setValue(newValue);
-      }}
-    >
-      <BottomNavigationAction label="Recents" icon={<AccessAlarmIcon />} />
-      <BottomNavigationAction label="Favorites" icon={<AccessAlarmIcon />} />
-      <BottomNavigationAction label="Nearby" icon={<AccessAlarmIcon />} />
-    </BottomNavigation>
+    <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction label="News" icon={<NewspaperIcon />} />
+        <BottomNavigationAction label="Jornada" icon={<DateRangeIcon />} />
+        <BottomNavigationAction label="Perfil" icon={<AccountCircleIcon />} />
+        <BottomNavigationAction label="Mensajes" icon={<CircleNotificationsIcon />} />
+      </BottomNavigation>
+    </Paper>
   );
 };
 
--- src/components/views/clubSelector/ClubSelector.tsx
+++ src/components/views/clubSelector/ClubSelector.tsx
@@ -1,38 +1,41 @@
 import { Button, Container, Grid } from '@mui/material';
-import BgImg from '../../../assets/img/intro-bg.svg'
+import Overlay from '../../../assets/img/white-overlay.svg'
 import React from 'react';
+
 import './ClubSelector.css'
 
 const ClubSelector = () => {
   return (
     <div id="ClubSelector">
+      <div className="bg-overlay-selector" style={{ backgroundImage: `url(${Overlay})`}}></div>
       <div className="main-content">
         <Container maxWidth="sm">
-          <h1>Selecciona un equipo</h1>
-          <p>Esta información nos servirá para ofrecerte el mejor contenido para ti</p>
-          <Grid container spacing={2}>
-            {
-              Array(15).fill(0).map((club, index) => {
-                return (
-                  <Grid key={index} item xs={4}>
-                    <div className="clubItem">
-                      <img src="https://res.cloudinary.com/dwta0wgpp/image/upload/v1645824762/asobal/14.svg"></img>
-                    </div>
-                  </Grid>
+          <div className="main-container">
+            <h1>Selecciona un equipo</h1>
+            <Grid container spacing={2} sx={{ mt: "1rem"}}>
+              {
+                Array(15).fill(0).map((club, index) => {
+                  return (
+                    <Grid key={index} item xs={4} sx={{ mb: '1.5rem'}}>
+                      <div className="clubItem">
+                        <img src="https://res.cloudinary.com/dwta0wgpp/image/upload/v1645824762/asobal/14.svg"></img>
+                      </div>
+                    </Grid>
+                  )
+                })
+              }
+            </Grid>
+              <Button
+                sx={{ mt: '3rem', py: "0.75rem", mb:'3rem' }}
+                fullWidth
+                variant="contained"
+                size="large"
+                type="submit"
+              >
+                Continuar
+              </Button>
+          </div>
 
-                )
-              })
-            }
-          </Grid>
-            <Button
-              sx={{ mt: '3rem', py: "0.75rem", mb:'2rem' }}
-              fullWidth
-              variant="contained"
-              size="large"
-              type="submit"
-            >
-              Continuar
-            </Button>
         </Container>
       </div>
     </div>
