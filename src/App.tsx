ed271cda22d49d4cd2b83624d9c96469f2035945
Cdelatorre <carlosdelatorrediaz@gmail.com>
2022-02-25 23:19:22 +0100

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   BrowserRouter,
   Routes,
@@ -8,20 +8,37 @@ import Login from './components/views/login/Login';
 import Register from './components/views/register/Register';
 import Intro from './components/views/intro/Intro';
 import ClubSelector from './components/views/clubSelector/ClubSelector';
+import Home from './components/views/home/Home';
+import { BottomNavigation, BottomNavigationAction } from '@mui/material';
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
 
 
 function App() {
+  const [value, setValue] = useState(0);
+
   return (
       <div className="App">
         <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<App />}/>
-              <Route path="login" element={<Login />} />
-              <Route path="register" element={<Register />} />
-              <Route path="intro" element={<Intro />} />
-              <Route path="club-selector" element={<ClubSelector />} />
-            </Routes>
-          </BrowserRouter>
+          <Routes>
+            <Route path="/" element={<App />}/>
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+            <Route path="intro" element={<Intro />} />
+            <Route path="club-selector" element={<ClubSelector />} />
+            <Route path="home" element={<Home />}/>
+          </Routes>
+        </BrowserRouter>
+        <BottomNavigation
+            showLabels
+            value={value}
+            onChange={(event, newValue) => {
+              setValue(newValue);
+            }}
+          >
+            <BottomNavigationAction label="Recents" icon={<AccessAlarmIcon />} />
+            <BottomNavigationAction label="Favorites" icon={<AccessAlarmIcon />} />
+            <BottomNavigationAction label="Nearby" icon={<AccessAlarmIcon />} />
+          </BottomNavigation>
       </div>
   );
 }
--- src/components/views/clubSelector/ClubSelector.tsx
+++ src/components/views/clubSelector/ClubSelector.tsx
@@ -1,11 +1,40 @@
+import { Button, Container, Grid } from '@mui/material';
+import BgImg from '../../../assets/img/intro-bg.svg'
 import React from 'react';
+import './ClubSelector.css'
 
 const ClubSelector = () => {
   return (
-    <div>
-      {/* {
-       Array(16).fill(0).map(())
-      } */}
+    <div id="ClubSelector">
+      <div className="main-content">
+        <Container maxWidth="sm">
+          <h1>Selecciona un equipo</h1>
+          <p>Esta información nos servirá para ofrecerte el mejor contenido para ti</p>
+          <Grid container spacing={2}>
+            {
+              Array(15).fill(0).map((club, index) => {
+                return (
+                  <Grid key={index} item xs={4}>
+                    <div className="clubItem">
+                      <img src="https://res.cloudinary.com/dwta0wgpp/image/upload/v1645824762/asobal/14.svg"></img>
+                    </div>
+                  </Grid>
+
+                )
+              })
+            }
+          </Grid>
+            <Button
+              sx={{ mt: '3rem', py: "0.75rem", mb:'2rem' }}
+              fullWidth
+              variant="contained"
+              size="large"
+              type="submit"
+            >
+              Continuar
+            </Button>
+        </Container>
+      </div>
     </div>
   );
 };
