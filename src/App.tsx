291f374439bbf47edc868a1d1dc5f4ffa7c0b403
Pablo Laso <plasocortabitarte@gmail.com>
2022-02-25 23:02:59 +0100

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,22 @@
-import React from 'react';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import Login from './components/views/login/Login';
-import Register from './components/views/register/Register';
-import Intro from './components/views/intro/Intro';
-import ClubSelector from './components/views/clubSelector/ClubSelector';
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/views/login/Login";
+import Register from "./components/views/register/Register";
+import Intro from "./components/views/intro/Intro";
+import ClubSelector from "./components/views/clubSelector/ClubSelector";
 
 function App() {
   return (
-      <div className="App">
-        <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<App />}/>
-              <Route path="login" element={<Login />} />
-              <Route path="register" element={<Register />} />
-              <Route path="intro" element={<Intro />} />
-              <Route path="club-selector" element={<ClubSelector />} />
-            </Routes>
-          </BrowserRouter>
-      </div>
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="intro" element={<Intro />} />
+          <Route path="club-selector" element={<ClubSelector />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
