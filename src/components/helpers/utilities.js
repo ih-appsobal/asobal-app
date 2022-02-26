908a0d678b48d10ef5ad1ff165861d170c90d620
Cdelatorre <carlosdelatorrediaz@gmail.com>
2022-02-26 10:27:26 +0100

    wip

---
+++ src/components/helpers/utilities.js
@@ -0,0 +1,17 @@
+
+
+export const formatDate = (date) => {
+  const dt = new Date(date)
+
+  return `${
+    (dt.getMonth()+1).toString().padStart(2, '0')}/${
+    dt.getDate().toString().padStart(2, '0')}/${
+    dt.getFullYear().toString().padStart(4, '0')} ${
+    dt.getHours().toString().padStart(2, '0')}:${
+    dt.getMinutes().toString().padStart(2, '0')}:${
+    dt.getSeconds().toString().padStart(2, '0')}`
+}
+
+export default {
+  formatDate,
+}
--- src/components/notifications/Notification/Notification.tsx
+++ src/components/notifications/Notification/Notification.tsx
@@ -1,14 +1,15 @@
 import React, { useState } from 'react';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
-import Divider from '@mui/material/Divider';
-import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
-import Typography from '@mui/material/Typography';
 import { Link } from 'react-router-dom';
-import './Notification.css';
 import NotificationPoint from '../NotificationPoint/NotificationPoint';
+import FromImg from '../../../assets/img/notification-from.jpg'
+import { formatDate } from '../../helpers/utilities';
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import './Notification.css';
 
 export interface NotificationProps {
     url: string,
@@ -22,7 +23,7 @@ const Notification = ({ url, read, message, position }: NotificationProps) => {
     console.log('positions =>', position === 0, position)
 
     const classCalculate =() => {
-        
+
         if(position === 0 || (position && position % 2 == 0)) {
             return 'bg-primary';
         } else {
@@ -36,13 +37,13 @@ const Notification = ({ url, read, message, position }: NotificationProps) => {
   return (
       <div className={`Notification ${backgroundNotification}`}>
         <Link to={url}>
-            <List sx={{ 
-                width: '100vw', 
+            <List sx={{
+                width: '100vw',
                 maxWidth: 360,
                 }}>
                 <ListItem alignItems="center">
                 <ListItemAvatar>
-                    {read && (
+                    {!read && (
                         <NotificationPoint />
                     )}
                     <Avatar
@@ -51,13 +52,21 @@ const Notification = ({ url, read, message, position }: NotificationProps) => {
                             height: '4rem',
                         }}
                         alt="Remy Sharp"
-                        src="/static/images/avatar/1.jpg" 
+                        src={FromImg}
                     />
                 </ListItemAvatar>
                    <div className='Notification-message'>
                        <p>{message}</p>
+                       <small>{formatDate(new Date())}</small>
                    </div>
                 </ListItem>
+                <div className="notification-actions">
+                    <RemoveRedEyeIcon
+                        // onClick={() => patch a read}
+                        className={`${read ? 'eye-disabled' : ''}`}
+                    />
+                    <DeleteOutlineIcon className=""/>
+                </div>
             </List>
         </Link>
     </div>
